a22db23d308cb438f26a118f50be996534d6cf84
junwon108 <37489628+junwon108@users.noreply.github.com>
2020-05-06 20:01:54 -0400

    Moved all GalleryFragment to Slideshow Fragment

--- app/src/main/java/com/example/canny418/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/canny418/ui/slideshow/SlideshowFragment.java
@@ -1,9 +1,18 @@
-package com.example.canny418.ui.slideshow;
-
+package com.example.canny418.ui.gallery;
+import android.app.Activity;
+import android.content.ContentProviderOperation;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,10 +22,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.canny418.R;
+import com.example.canny418.ui.slideshow.SlideshowViewModel;
+
+import java.io.ByteArrayOutputStream;
 
 public class SlideshowFragment extends Fragment {
 
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1888;
+
     private SlideshowViewModel slideshowViewModel;
+
+    ImageView imageView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,6 +44,16 @@ public class SlideshowFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+
+        final Button button = root.findViewById(R.id.button_start_camera);
+        imageView = (ImageView) root.findViewById(R.id.video);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent i = new Intent(getActivity(), RealTimeActivity.class);
+                startActivity(i);
             }
         });
         return root;
